21ec4d6fe1fcf588e3adf5da635afa1724caacf2
KanapongP <munticry@gmail.com>
2019-05-18 21:14:00 +0700

    add laura3,4,5

---
+++ Token4.py
@@ -0,0 +1,25 @@
+import deepcut
+openfile = open("./NLp_Project/sentence4.txt","r", encoding='utf-8-sig')
+opendict = open("./NLp_Project/Destination.txt","r",encoding='utf-8-sig')
+openout = open("./NLp_Project/laura4.txt","w",encoding='utf-8-sig')
+opendict.readline()
+dict = []
+for line in opendict:
+    for word in line.rstrip().split(','):
+        dict.append(word)
+dataset = [line.strip() for line in openfile]
+word = [[w for w in deepcut.tokenize(data, dict + ['ไป','-'])]for data in dataset]
+for w in word :
+    if len(w)!= 3 :
+        print(w)
+for x in word :
+    for i,y in enumerate(x):
+        if i == 0 :
+            openout.writelines(y + " " + "B-DEST"+"\n")
+        elif i == 2 :
+            openout.writelines(y +" " + "B-START"+"\n")
+        else :
+            openout.writelines(y + " " + "O"+"\n") 
+    openout.writelines('\n')
+
+
---
+++ Token5.py
@@ -0,0 +1,25 @@
+import deepcut
+openfile = open("./NLp_Project/sentence5.txt","r", encoding='utf-8-sig')
+opendict = open("./NLp_Project/Destination.txt","r",encoding='utf-8-sig')
+openout = open("./NLp_Project/laura5.txt","w",encoding='utf-8-sig')
+opendict.readline()
+dict = []
+for line in opendict:
+    for word in line.rstrip().split(','):
+        dict.append(word)
+dataset = [line.strip() for line in openfile]
+word = [[w for w in deepcut.tokenize(data, dict + ['ไป','-'])]for data in dataset]
+for w in word :
+    if len(w)!= 3 :
+        print(w)
+for x in word :
+    for i,y in enumerate(x):
+        if i == 0 :
+            openout.writelines(y + " " + "B-START"+"\n")
+        elif i == 2 :
+            openout.writelines(y +" " + "B-DEST"+"\n")
+        else :
+            openout.writelines(y + " " + "O"+"\n") 
+    openout.writelines('\n')
+
+
